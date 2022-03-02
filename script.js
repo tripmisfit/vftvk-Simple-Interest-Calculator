8e01bf307939ae3645cc3fead1729b2d2df2536c
orion urrego <Orionaurrego0926@gmail.com>
2022-03-02 05:00:54 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute()  
+
+ 
+ 
+
+{  
+
+ 
+ 
+
+    p = document.getElementById("principal").value;  
+
+ 
+ 
+
+    var principal = document.getElementById("principal").value;  
+
+ 
+ 
+
+    var rate = document.getElementById("rate").value;  
+
+ 
+ 
+
+    var years = document.getElementById("years").value;  
+
+ 
+ 
+
+    var interest = principal * years * rate/100;  
+
+ 
+ 
+
+    var year = new Date().getFullYear()+parseInt(years);  
+
+ 
+ 
+
+    function updaterate()  
+
+ 
+ 
+
+    {  
+
+ 
+ 
+
+        var rateval = document.getElementById("rate").value;  
+
+ 
+ 
+
+        document.getElementById("rate_val").innerText=rateval;  
+
+ 
+ 
+
+    }  
+
+ 
+ 
+
+    function updateRate() 
+    {    var rateval =
+    document.getElementById("rate").value;  
+    document.getElementById("rate_val").innerText=rateval;
+    } 
 }
-        
